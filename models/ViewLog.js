e36b949c164814787d353b4df2ba951fa6c292c4
taha <taha@dmdmax.com>
2021-09-01 13:55:42 +0500

    viewlogs model updated

--- models/ViewLog.js
+++ models/ViewLog.js
@@ -9,6 +9,8 @@ const viewLogSchema = new Schema({
     _id: { type: ShortId, len: 16, retries: 4 },
     user_id: { type: String, required: true},
     subscription_id: {type: ShortId},
+    source: {type: String, default: 'app', index: true},
+    operator: {type: String, default: 'telenor', index: true},
     added_dtm: {type: Date, default: new Date()}
 }, { strict: true });
 
